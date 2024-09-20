a7332f44bef355a003b3ee7593936dbfbbbddc37
aengzu <jeon7879@naver.com>
2024-09-20 15:14:53 +0900

    Design: Component 샘플 업로드

---
+++ core/designsystem/src/main/java/com/iguana/designsystem/sample/ComponentSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.iguana.designsystem.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iguana.designsystem.R
+
+class ComponentSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_component_sample)
+    }
+}
