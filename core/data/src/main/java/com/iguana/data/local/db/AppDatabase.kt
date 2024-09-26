8f5a80c3aeedeabe7bf6fc54ee1d7ae7333e4865
Kjamm <happyminig@gmail.com>
2024-09-26 22:33:22 +0900

    Feat : Repository 구현

--- core/data/src/main/java/com/iguana/data/local/db/AppDatabase.kt
+++ core/data/src/main/java/com/iguana/data/local/db/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.iguana.data.local.dao.RecentFileDao
 import com.iguana.data.local.entity.RecentFileEntity
 
 @Database(entities = [RecentFileEntity::class], version = 1, exportSchema = false)
