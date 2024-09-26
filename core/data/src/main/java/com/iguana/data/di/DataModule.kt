1f4ba3e9d73f1365418f53dab762105619f2de27
Kjamm <happyminig@gmail.com>
2024-09-26 22:31:27 +0900

    Feat : DAO 정의

--- core/data/src/main/java/com/iguana/data/di/DataModule.kt
+++ core/data/src/main/java/com/iguana/data/di/DataModule.kt
@@ -1,18 +1,25 @@
 package com.iguana.data.di
 
+import android.content.Context
+import androidx.room.Room
+import com.iguana.data.local.dao.RecentFileDao
+import com.iguana.data.local.db.AppDatabase
 import com.iguana.data.repository.LoginRepositoryImpl
 import com.iguana.domain.repository.LoginRepository
 import com.iguana.domain.repository.SharedPreferencesHelper
 import com.iguana.data.local.db.SharedPreferencesHelperImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataModule {
+
     @Binds
     @Singleton
     abstract fun bindLoginRepository(
@@ -24,4 +31,21 @@ abstract class DataModule {
     abstract fun bindSharedPreferencesHelper(
         sharedPreferencesHelperImpl: SharedPreferencesHelperImpl
     ): com.iguana.domain.repository.SharedPreferencesHelper
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "app_database"
+            ).build()
+        }
+
+        @Provides
+        fun provideRecentFileDao(appDatabase: AppDatabase): RecentFileDao {
+            return appDatabase.recentFileDao()
+        }
+    }
 }
