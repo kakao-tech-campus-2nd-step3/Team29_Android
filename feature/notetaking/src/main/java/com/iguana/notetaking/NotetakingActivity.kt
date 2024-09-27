1664c445fc7e543c251a70ddd8637fe1b9c3c777
aengzu <jeon7879@naver.com>
2024-09-28 02:04:34 +0900

    Feat: 사이드바 슬라이더 효과 구현

--- feature/notetaking/src/main/java/com/iguana/notetaking/NotetakingActivity.kt
+++ feature/notetaking/src/main/java/com/iguana/notetaking/NotetakingActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.iguana.notetaking.databinding.ActivityNotetakingBinding
 import com.iguana.notetaking.databinding.ActivitySampleFilePickerBinding
@@ -14,19 +15,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NotetakingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNotetakingBinding
+    private val viewModel: NotetakingViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityNotetakingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
         // Intent에서 PDF URI 받기
         val pdfUriString = intent.getStringExtra("PDF_URI")
         val pdfTitle = intent.getStringExtra("PDF_TITLE") ?: "무제"
 
         binding.titleBar.titleBar.text = pdfTitle
-
-
 
         if (pdfUriString != null) {
             val pdfUri = Uri.parse(pdfUriString)
