30550130ba8f2ce5efd8e1cbf52384fbd4e4aa6b
aengzu <jeon7879@naver.com>
2024-10-04 19:09:14 +0900

    Feat: Annotation Mapper 함수 제작

---
+++ core/data/src/main/java/com/iguana/data/mapper/AnnotationMapper.kt
@@ -0,0 +1,34 @@
+package com.iguana.data.mapper
+
+import com.iguana.data.remote.model.*
+import com.iguana.domain.model.Annotation
+
+
+fun AnnotationResponseDto.toDomain() = Annotation(
+    id = id,
+    content = content,
+    x = x,
+    y = y,
+    width = width,
+    height = height
+)
+
+fun GetAnnotationsResponseDto.toDomain() = annotations.map { it.toDomain() }
+
+fun Annotation.toCreateAnnotationRequestDto(documentId: Long, pageNumber: Int) =
+    CreateAnnotationRequestDto(
+        pageNumber = pageNumber,
+        x = x,
+        y = y,
+        width = width,
+        height = height,
+        content = content
+    )
+
+fun Annotation.toUpdateAnnotationRequestDto() = UpdateAnnotationRequestDto(
+    content = content,
+    x = x,
+    y = y,
+    width = width,
+    height = height
+)
