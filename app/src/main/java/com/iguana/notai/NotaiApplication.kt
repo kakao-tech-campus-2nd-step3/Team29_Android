976f5b4eb712cda78dae56b794dbb88da3d3b506
aengzu <jeon7879@naver.com>
2024-11-13 21:50:54 +0900

    Feat: STT api, dto구현

--- app/src/main/java/com/iguana/notai/NotaiApplication.kt
+++ app/src/main/java/com/iguana/notai/NotaiApplication.kt
@@ -1,7 +1,9 @@
 package com.iguana.notai
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,5 +11,9 @@ class NotaiApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, getString(R.string.kakao_native_key))
+
+        // 키해시를 구해서 로그에 출력
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("KeyHash", "KeyHash: $keyHash")
     }
 }
