dd60c69891bf3e8d4108ec40d8a042642682481d
aengzu <jeon7879@naver.com>
2024-11-15 16:43:04 +0900

    Fix: Callback import 에러 수정

--- core/navigation/build/tmp/kapt3/stubs/release/com/iguana/navigation/LoginNavigator.java
+++
@@ -1,7 +0,0 @@
-package com.iguana.navigation;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/iguana/navigation/LoginNavigator;", "", "navigateToLogin", "", "navigation_release"})
-public abstract interface LoginNavigator {
-    
-    public abstract void navigateToLogin();
-}
