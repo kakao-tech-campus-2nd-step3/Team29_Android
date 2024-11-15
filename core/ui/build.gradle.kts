5b06d28f9310cee25fb420381c6bc15d718c65a4
aengzu <jeon7879@naver.com>
2024-11-15 17:26:11 +0900

    Fix: 빌드 에러 수정

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -16,7 +16,6 @@ android {
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(projects.core.designsystem)
-    implementation(projects.core.navigation)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
